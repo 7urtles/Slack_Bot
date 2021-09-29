837d8976a7d4f888ab0479d9716436af5242c495
charles <charleshparmley@icloud.com>
2021-09-28 20:20:19 -0500

    working state

--- Newest_Assignement.py
+++ Newest_Assignement.py
@@ -15,46 +15,63 @@ class Populi_Bot():
         driver = webdriver.Firefox(firefox_options=options)
 
         # Navigate to assignments page
+        print("Opening Populi")
         driver.get("https://dfa.populiweb.com/router/courseofferings/10734937/assignments/index")
         sleep(1)
 
         # Enter username in google login form
+        print('Entering google user')
         elem = driver.find_element_by_id("identifierId")
         elem.send_keys(username)
         elem.send_keys(Keys.RETURN)
-        sleep(1)
+        sleep(2)
 
         # Enter password in google login form
+        print('Entering google password')
         elem = driver.find_element_by_name("password")
         elem.send_keys(password)
         elem.send_keys(Keys.RETURN)
-        sleep(4)
+        sleep(5)
 
         # Grab all the <a></a> tags with a class name of 'remote_name' (assingment links have this class name)
+        print('Finding Assignments')
         elem = driver.find_elements_by_class_name("remote_nav")
         
-        # Grab the second to last assignment link (This is the newest assignement issued)
-        link = elem[-2].get_attribute("href")
-        title = elem[-2].get_attribute("innerHTML")
-        # Do not close the driver before the 2 'elem' commands above or program fails
+        # Add all found assignments to a list
+        found_assignments = []
+        for item in elem:
+            # print('Link added')
+            title = item.get_attribute("innerHTML")
+            link = item.get_attribute("href")
+            found_assignments.append([title,link])
+
+        # Close the webdriver after done using its functions
         driver.close()
+        driver.quit()
+
 
         f = open("Assignment_List.txt", "r")
         assignments = f.readlines()
         f.close()
 
-        # Check assignment file for already existing homework link
-        for assignment in assignments:
-            # If it already exists exit the function
-            if link in assignment:
-                return False
         
+        # Check assignment file for already existing homework link
+        for item in found_assignments:
+            flag = True
+            for assignment in assignments:
+                title = item[0]
+                link = item[1]
+                # print(link, '\n', assignment)
+                if link in assignment:
+                    # print('Assignment already exists.')
+                    flag = False
+                    break
+            if flag == True:
+                # Add the new assignment to the assignments file
+                print("Adding new assignment to file")
+                f = open("Assignment_List.txt", "a")
+                f.write(f'{title} {link}\n')
+                f.close()
 
-        # Add the new assignment to the assignments file
-        f = open("Assignment_List.txt", "a")
-        f.write(f'{title} {link}\n')
-        f.close()
-
-        # Pass the homework description and link back to the slack bot for posting
-        return title,link
-
+                # Pass the homework description and link back to the slack bot for posting
+                return title,link
--- assignment_bot.py
+++ assignment_bot.py
@@ -1,18 +1,29 @@
 slack_token = 'xoxb-2470203358324-2551145549889-kkOPq1COylop2HP7V2GDKqLA'
 import slack
 from Newest_Assignement import Populi_Bot
+import multiprocessing as mp
+import os
+
 
 def post_new_assignemnt():
     client = slack.WebClient(slack_token)
     try:
+        print('Checking for assignments')
         latest_assignment_title,latest_assignemt_link = Populi_Bot.assignment_checker()
         print('New assignment found')
     except:
         print('No new assignments')
         return
-
+    # Post the message
     message = f'{latest_assignment_title} --> {latest_assignemt_link}'
-    client.chat_postMessage(channel='#bot_testing', text=message)
+    client.chat_postMessage(channel='#general', text=message)
 
 while True:
-    post_new_assignemnt()
+    p = mp.Process(target=post_new_assignemnt())
+    # run `worker` in a subprocess
+    p.start()
+    # make the main process wait for `worker` to end
+    p.join()
+    # all memory used by the subprocess will be freed to the OS
+    os.system('pkill firefox')
+    
